70ca6a935b5a04701cf8c16a97a01a417f59578e
chodayoung <verysweetkey@gmail.com>
2025-10-31 21:12:32 +0900

    feat : 공통 컴포넌트 checkbox,radio 일부 추가

---
+++ src/components/ui/CheckBox.tsx
@@ -0,0 +1,35 @@
+import type { CheckboxAloneProps, CheckboxProps } from '@/components/types/input';
+
+export function CheckBox({ id, label, checked, onChange, className }: CheckboxProps) {
+  return (
+    <>
+      <label
+        htmlFor={id}
+        className={`flex cursor-pointer items-center gap-2 text-lg text-(--color-primary-700)`}
+      >
+        <input
+          id={id}
+          type='checkbox'
+          checked={checked}
+          onChange={onChange}
+          className={`mr-2 h-4 w-4 rounded-lg border border-(--color-primary-500-50) accent-(--color-primary-500-50) ${className}`}
+        />
+        {label}
+      </label>
+    </>
+  );
+}
+
+export function CheckBoxAlone({ id, checked, onChange, className }: CheckboxAloneProps) {
+  return (
+    <>
+      <input
+        id={id}
+        type='checkbox'
+        checked={checked}
+        onChange={onChange}
+        className={`mr-2 h-4 w-4 rounded-lg border border-(--color-primary-500-50) accent-(--color-primary-500-50) ${className}`}
+      />
+    </>
+  );
+}
