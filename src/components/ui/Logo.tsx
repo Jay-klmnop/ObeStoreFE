c3095978597d07d036535b840dc1f5849b511c6a
chodayoung <verysweetkey@gmail.com>
2025-11-03 10:31:19 +0900

    feat : 공통 컴포넌트 logo파일 업로드, logo.tsx 삭제

--- src/components/ui/Logo.tsx
+++
@@ -1,8 +0,0 @@
-export default function Logo() {
-  return (
-    <div>
-      {/* /Users/a/Desktop/OBE-STORE/src/components/icon/HeaderLogoIcon.tsx
-      /Users/a/Desktop/OBE-STORE/src/components/icon/HeaderLogoImgIcon.tsx 로고는 여기에 있뜸여 */}
-    </div>
-  );
-}
