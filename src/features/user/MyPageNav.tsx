5c36ae51305a8bbb5ed0d2a820d0d880ad0609af
Jay <jiyeyoun@outlook.com>
2025-11-03 19:05:41 +0900

    fix: MyPageNav 오타 수정

--- src/features/user/MyPageNav.tsx
+++ src/features/user/MyPageNav.tsx
@@ -1,6 +1,6 @@
 import { MYPAGE_NAV_LINKS } from '@/constants';
 
-export function HeaderNav() {
+export function MyPageNav() {
   return (
     <div className='flex flex-col items-center gap-4'>
       {MYPAGE_NAV_LINKS.map(({ label }) => (
