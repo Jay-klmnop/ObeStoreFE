bcc53a4525d8e6a449b08be3cd03bf3302870cd0
Jay <jiyeyoun@outlook.com>
2025-11-03 19:03:05 +0900

    feat: 마이페이지 네비게이션 바 추가 (#44)

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -34,23 +34,46 @@ export const HEADER_ICONS_LINKS: HeaderIconLink[] = [
   },
   {
     type: 'link',
-    href: '/mypage',
+    href: '/users',
     label: 'My Page',
     Icon: ProfileIcon,
     responsiveClass: 'mobile-visible',
   },
   {
     type: 'link',
-    href: '/mypage/favorites',
+    href: '/users/favorites',
     label: 'Favorites',
     Icon: HeartIcon,
     responsiveClass: 'mobile-hidden',
   },
   {
     type: 'link',
-    href: '/mypage/cart',
+    href: '/users/cart',
     label: 'Cart',
     Icon: CartIcon,
     responsiveClass: 'mobile-hidden',
+  },
+];
+
+export const MYPAGE_NAV_LINKS = [
+  {
+    type: 'link',
+    href: '/orders',
+    label: '주문 내역',
+  },
+  {
+    type: 'link',
+    href: '/users/me',
+    label: '나의 정보 조회/수정',
+  },
+  {
+    type: 'link',
+    href: '/users/me', // 확실하지 않아서 확인해야함
+    label: '배송지 정보 조회/수정',
+  },
+  {
+    type: 'action',
+    action: 'toggleSignout',
+    label: '로그아웃',
   },
 ];
