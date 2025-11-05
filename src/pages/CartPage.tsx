e31bbfc16fd22ce47d7a0bcd8556252b295613e1
chodayoung <verysweetkey@gmail.com>
2025-11-05 17:32:12 +0900

    feat:  cart 기능 업데이트 & zustand 연결

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,9 +1,11 @@
+import { Subtitle } from '@/components/ui/Subtitle';
 import CartList from '@/features/cart/CartList';
 
 export default function CartPage() {
   return (
     <>
       <section className='bg-primary-500-0 w-full px-3.5 py-4.5'>
+        <Subtitle label='장바구니' />
         <CartList />
       </section>
     </>
