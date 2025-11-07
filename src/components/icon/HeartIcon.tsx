77ac860ffa8aa3c0b7bdbfcd34fa13fc891e85fd
Jay <jiyeyoun@outlook.com>
2025-11-07 18:39:13 +0900

    feat: 찜 기능 구현 (#104)

--- src/components/icon/HeartIcon.tsx
+++ src/components/icon/HeartIcon.tsx
@@ -1,6 +1,10 @@
 import type { IconProps } from '@/components/types';
-import { GoHeart } from 'react-icons/go';
+import { GoHeart, GoHeartFill } from 'react-icons/go';
 
-export function HeartIcon({ size, color, className }: IconProps) {
+export function EmptyHeartIcon({ size, color, className }: IconProps) {
   return <GoHeart size={size} strokeWidth={0.6} color={color} className={className} />;
+}
+
+export function FilledHeartIcon({ size, color, className }: IconProps) {
+  return <GoHeartFill size={size} strokeWidth={0.6} color={color} className={className} />;
 }
--- src/features/product/ProductCard.tsx
+++ src/features/product/ProductCard.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
-import { HeartIcon } from '@/components/icon';
 import { ReviewRating } from '@/components/ui';
 import type { ProductCardType } from '@/types';
+import { FavoriteIcon } from '@/features/favorite';
 
 interface ProductCardProps {
   product: ProductCardType;
@@ -10,27 +10,23 @@ interface ProductCardProps {
 export function ProductCard({ product }: ProductCardProps) {
   return (
     <Link to={`/products/${product.id}`}>
-      <div className='flex max-h-96 min-h-80 min-w-60 flex-col items-center justify-center gap-4 rounded-lg p-4 text-center opacity-90 transition-opacity duration-300 ease-in-out'>
+      <div className='flex max-h-96 min-h-80 min-w-60 flex-col items-center justify-center gap-4 p-4 text-center opacity-90 transition-opacity duration-300 ease-in-out'>
         <div className='relative flex h-full w-full flex-col'>
           <img
-            src={product.product_image.thumbnail}
+            src={product.product_image.product_card_image}
             alt={product.product_name}
-            className='h-auto w-full min-w-32 rounded-lg object-cover object-top'
+            className='h-auto w-full min-w-32 object-cover object-top'
             width={300}
             height={400}
           />
-          <HeartIcon
-            size={20}
-            color='var(--color-secondary-300)'
-            className='absolute right-2 bottom-2'
-          />
+          <FavoriteIcon product={product} />
         </div>
 
         <div className='flex h-14 grow flex-col justify-evenly text-xs'>
-          <h2>{product.product_brand}</h2>
+          <h2>{product.brand_name}</h2>
           <h3 className='mb-2 font-bold'>{product.product_name}</h3>
           <div className='flex justify-between'>
-            <p className='font-bold'>₩{Number(product.product_price).toLocaleString('ko-KR')}</p>
+            <p className='font-bold'>₩{Number(product.dc_value).toLocaleString('ko-KR')}</p>
             <ReviewRating initialValue={Number(product.product_rating)} readOnly size={12} />
           </div>
         </div>
