0b282c8e17320046a81ba3d347744b8a15b350b3
chodayoung <verysweetkey@gmail.com>
2025-11-10 23:42:48 +0900

    feat: 배송지 관리 일부 업데이트 및 버튼 변경

--- src/features/mypage/components/MypageContentsWrap.tsx
+++ src/features/mypage/components/MypageContentsWrap.tsx
@@ -4,5 +4,5 @@ type MypageContentsWrapProps = {
   children: ReactNode;
 };
 export function MypageContentsWrap({ children }: MypageContentsWrapProps) {
-  return <div className='mt-7.5 w-full lg:mt-15 lg:ml-7 lg:w-[calc(100%-230px)]'>{children}</div>;
+  return <div className='mt-7.5 w-full lg:mt-15'>{children}</div>;
 }
