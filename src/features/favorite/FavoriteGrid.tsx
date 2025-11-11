c23403447f914159813d1bd352f3db0c038fa8a3
Jay <jiyeyoun@outlook.com>
2025-11-12 06:49:08 +0900

    feat: FavoritesPage 추가 (#159)

--- src/features/favorite/FavoriteGrid.tsx
+++ src/features/favorite/FavoriteGrid.tsx
@@ -5,7 +5,7 @@ export function FavoriteGrid() {
   const { favoriteProducts } = useFavoriteStore();
 
   return (
-    <div className='mx-4 my-4 grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-4 self-stretch'>
+    <div className='m-4 grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-4 self-stretch'>
       {favoriteProducts.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
