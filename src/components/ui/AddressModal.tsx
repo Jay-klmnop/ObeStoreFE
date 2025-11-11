8f2f7c903f0ee94e7f1bf7f9e16b88bbfb9156da
chodayoung <verysweetkey@gmail.com>
2025-11-11 14:35:25 +0900

    feat: 배송지 관리 기능 구현 수정,삭제 추가

--- src/components/ui/AddressModal.tsx
+++ src/components/ui/AddressModal.tsx
@@ -1,9 +1,17 @@
 import { useAddressModalStore } from '@/store/useAddressModalStore';
 import { AddressForm } from './AddressForm';
+import { useAddressMutation } from '@/features/mypage/api/useAddressQuery';
+import { ConfirmModal } from './ConfirmModal';
 
 export function AddressModal() {
-  const { isOpen, closeModal, editingAddress } = useAddressModalStore();
-
+  const { isOpen, mode, closeModal, editingAddress } = useAddressModalStore();
+  const { deleteAddress } = useAddressMutation();
+  const handleDeleteConfirm = () => {
+    if (!editingAddress) return;
+    deleteAddress.mutate(editingAddress.id, {
+      onSuccess: () => closeModal(),
+    });
+  };
   if (!isOpen) return null;
 
   return (
@@ -12,12 +20,30 @@ export function AddressModal() {
     >
       <div className='w-[500px] rounded-lg bg-white p-6'>
         <div className='mb-4 flex items-center justify-between border-b pb-2'>
-          <h2 className='text-lg font-bold'> {editingAddress ? '배송지 수정' : '배송지 추가'}</h2>
+          <h2 className='text-lg font-bold'>
+            {' '}
+            {mode === 'add' ? '배송지 추가' : mode === 'edit' ? '배송지 수정' : '배송지 삭제'}
+          </h2>
           <button className='text-3xl font-light' aria-label='닫기' onClick={closeModal}>
             &times;
           </button>
         </div>
-        <AddressForm />
+
+        {mode === 'delete' ? (
+          <ConfirmModal
+            isOpen={true}
+            closeModal={closeModal}
+            onConfirm={handleDeleteConfirm}
+            onCancel={closeModal}
+            buttons={true}
+          >
+            <p className='text-center text-gray-700'>
+              <strong>{editingAddress?.name}</strong> 배송지를 정말 삭제하시겠습니까?
+            </p>
+          </ConfirmModal>
+        ) : (
+          <AddressForm />
+        )}
       </div>
     </div>
   );
