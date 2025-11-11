3ba4b9ad4fbf718a5cbc6dab51e262cf5e2e6542
Jay <jiyeyoun@outlook.com>
2025-11-12 06:01:51 +0900

    feat: 헤더에 모바일용 메뉴 추가 (#154)

---
+++ src/components/layout/header/HeaderMobileNav.tsx
@@ -0,0 +1,24 @@
+import { HEADER_ICONS_LINKS, HEADER_NAV_LINKS, type HeaderIconLinkType } from '@/constants';
+import { useToggleMenuStore } from '@/store';
+import { Link } from 'react-router-dom';
+
+export function HeaderMobileNav() {
+  const closeMenu = useToggleMenuStore((state) => state.closeMenu);
+  return (
+    <div className='flex flex-col items-center gap-8 text-lg font-black md:hidden'>
+      {HEADER_NAV_LINKS.map(({ label, href }) => (
+        <Link key={label} to={href} onClick={closeMenu}>
+          {label}
+        </Link>
+      ))}
+      {HEADER_ICONS_LINKS.filter(
+        (link): link is Extract<HeaderIconLinkType, { type: 'link' }> =>
+          link.type === 'link' && link.responsiveClass === 'mobile-hidden'
+      ).map((link) => (
+        <Link key={link.label} to={link.href} onClick={closeMenu}>
+          {link.label}
+        </Link>
+      ))}
+    </div>
+  );
+}
