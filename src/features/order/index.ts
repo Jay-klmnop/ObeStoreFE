62760409a9e664b08dacd82b9f6f8a474d2d0a55
chodayoung <verysweetkey@gmail.com>
2025-11-11 10:39:30 +0900

    fix: index.ts 파일에 맞게 수정, input 클래스 조정

--- src/features/order/index.ts
+++ src/features/order/index.ts
@@ -1,4 +1,7 @@
 export * from './OrderCard';
+export * from './OrderCheckoutPage';
+export * from './OrderFail';
 export * from './OrderList';
 export * from './OrderSideBar';
+export * from './OrderSuccess';
 export * from './OrderSuccessModal';
