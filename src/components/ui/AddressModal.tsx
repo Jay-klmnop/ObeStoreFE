9a863942269c5315dce8fa776521ba203ea6aec2
chodayoung <verysweetkey@gmail.com>
2025-11-13 10:11:37 +0900

    feat: 배송지 관리 선택 기능 추가 및 Order에 연결

--- src/components/ui/AddressModal.tsx
+++ src/components/ui/AddressModal.tsx
@@ -1,35 +1,64 @@
 import { useAddressModalStore } from '@/store/useAddressModalStore';
 import { AddressForm } from './AddressForm';
-import { useAddressMutation } from '@/features/mypage/api/useAddressQuery';
+import { useAddressMutation, useAddressQuery } from '@/features/mypage/api/useAddressQuery';
 import { ConfirmModal } from './ConfirmModal';
+import { AddressList } from './AddressList';
+import { useEffect } from 'react';
 
-export function AddressModal() {
+export function AddressModal({ onSelectAddress }: { onSelectAddress?: (addr: any) => void }) {
   const { isOpen, mode, closeModal, editingAddress } = useAddressModalStore();
   const { deleteAddress } = useAddressMutation();
+  const { data: addresses = [] } = useAddressQuery();
+
   const handleDeleteConfirm = () => {
     if (!editingAddress) return;
     deleteAddress.mutate(editingAddress.id, {
       onSuccess: () => closeModal(),
     });
   };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
-    <div
-      className={`centralize bg-primary-700/50 fixed inset-0 z-100 h-full w-full backdrop-blur-sm`}
-    >
-      <div className='w-[500px] rounded-lg bg-white p-6'>
-        <div className='mb-4 flex items-center justify-between border-b pb-2'>
+    <div className='centralize bg-primary-700/50 fixed inset-0 z-90 flex h-full w-full items-center justify-center overflow-hidden backdrop-blur-sm'>
+      <div
+        className={`${mode === 'select' ? 'h-[530px]' : ''} w-[800px] items-center overflow-y-auto rounded-lg bg-white p-6`}
+      >
+        <div className='pb-2∂ mb-4 flex items-center justify-between border-b'>
           <h2 className='text-lg font-bold'>
             {' '}
-            {mode === 'add' ? '배송지 추가' : mode === 'edit' ? '배송지 수정' : '배송지 삭제'}
+            {mode === 'add'
+              ? '배송지 추가'
+              : mode === 'edit'
+                ? '배송지 수정'
+                : mode === 'delete'
+                  ? '배송지 삭제'
+                  : '배송지 선택'}
           </h2>
           <button className='text-3xl font-light' aria-label='닫기' onClick={closeModal}>
             &times;
           </button>
         </div>
-
-        {mode === 'delete' ? (
+        {mode === 'select' ? (
+          <AddressList
+            addresses={addresses}
+            onSelect={(addr) => {
+              onSelectAddress?.(addr); //
+              closeModal();
+            }}
+          />
+        ) : mode === 'delete' ? (
           <ConfirmModal
             isOpen={true}
             closeModal={closeModal}
--- src/pages/mypage/MyPageAddressInfo.tsx
+++ src/pages/mypage/MyPageAddressInfo.tsx
@@ -4,6 +4,7 @@ import { ButtonBase } from '@/components/ui/button';
 import { useAddressQuery } from '@/features/mypage/api/useAddressQuery';
 import { useAddressModalStore } from '@/store/useAddressModalStore';
 import { AddressModal } from '@/components/ui/AddressModal';
+import { AddressList } from '@/components/ui/AddressList';
 
 export function MyPageAddressInfo() {
   const { data: addresses = [] } = useAddressQuery();
@@ -18,36 +19,7 @@ export function MyPageAddressInfo() {
               배송지 추가하기
             </ButtonBase>
           </div>
-          {addresses.map((addrinfo) => (
-            <div
-              key={addrinfo.id}
-              className='border-primary-500-20 bg-primary-500-0 text-custom-gray-70 mt-5 rounded-lg border p-3 pt-2 text-lg leading-7'
-            >
-              <div className='flex items-center font-bold'>
-                <span>{addrinfo.name}</span>
-                {addrinfo.isDefault && (
-                  <span className='text-primary-500-90 border-primary-500-40 ml-2 rounded border bg-white/60 p-1 text-xs'>
-                    기본 배송지
-                  </span>
-                )}
-              </div>
-              <div className='text-1xl font-bold text-black'>{addrinfo.name}</div>
-              <div className='mt-2'>
-                {addrinfo.address} {addrinfo.detail}
-              </div>
-              <div>{addrinfo.phone}</div>
-              <div className='border-red flex justify-end gap-2'>
-                <ButtonBase
-                  variant='gnb'
-                  className='w-15 self-end'
-                  onClick={() => openModal('edit', addrinfo)}
-                >
-                  수정
-                </ButtonBase>
-                <ButtonBase onClick={() => openModal('delete', addrinfo)}>삭제</ButtonBase>
-              </div>
-            </div>
-          ))}
+          <AddressList addresses={addresses} />
         </div>
         <AddressModal />
       </MypageContentsWrap>
