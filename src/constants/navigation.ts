dc4e0c72f029e1c8945aea1ba1ccebf0d12eeb37
DESKTOP-SPQ41UM\user <jiyeyoun@outlook.com>
2025-11-14 04:14:59 +0900

    chore: 안 쓰는 코드/파일 삭제 (#199)

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -56,26 +56,3 @@ export const HEADER_ICONS_LINKS: HeaderIconLinkType[] = [
     responsiveClass: 'mobile-hidden',
   },
 ];
-
-export const MYPAGE_NAV_LINKS = [
-  {
-    type: 'link',
-    href: '/orders',
-    label: '주문 내역',
-  },
-  {
-    type: 'link',
-    href: '/users/me',
-    label: '나의 정보 조회/수정',
-  },
-  {
-    type: 'link',
-    href: '/users/me', // 확실하지 않아서 확인해야함
-    label: '배송지 정보 조회/수정',
-  },
-  {
-    type: 'action',
-    action: 'toggleSignout',
-    label: '로그아웃',
-  },
-];
