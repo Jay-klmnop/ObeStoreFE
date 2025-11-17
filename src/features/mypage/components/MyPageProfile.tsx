2d88ed3b69562bb167e17bd371d791cc16cf3374
Jay <jiyeyoun@outlook.com>
2025-11-17 23:14:34 +0900

    chore: 안쓰는 파일들 정리 + 재사용성 있게 파일명 수정 (#272)

--- src/features/mypage/components/MyPageProfile.tsx
+++ src/features/mypage/components/MyPageProfile.tsx
@@ -1,12 +1,19 @@
-import { MypageProfileIcon } from '@/components/icon';
+import { UserProfileIcon } from '@/components/icon';
+import { ErrorMessage, Spinner } from '@/components/ui';
+import { useUserQuery } from '@/features/mypage';
 import { Link } from 'react-router-dom';
 
 export function MyPageProfile() {
+  const { data: user, isLoading, error } = useUserQuery();
+
+  if (isLoading) return <Spinner />;
+  if (error) return <ErrorMessage />;
+
   return (
     <div className='p-4 lg:p-8'>
-      <Link to='/mypage/info' className='flex items-center gap-4'>
-        <MypageProfileIcon />
-        <p className='text-lg font-bold'>Nickname</p>
+      <Link to='/users' className='flex items-center gap-4'>
+        <UserProfileIcon />
+        <p className='text-lg font-bold'>{user?.nickname}</p>
       </Link>
     </div>
   );
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,2 @@
-export * from './dummyjson';
 export * from './product';
 export * from './order';
