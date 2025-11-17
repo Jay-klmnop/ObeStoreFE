14737b14f6833502b449d8a98b220dccc932be0c
Jay-klmnop <jiyeyoun@outlook.com>
2025-11-17 15:22:24 +0000

    Merge pull request #280 from oz-main-13-team3/feature/263-edit-deliveryRequest,detail
    fix: 코드 충돌 해결

--- src/features/mypage/components/index.ts
+++ src/features/mypage/components/index.ts
@@ -1,5 +1,6 @@
 export * from './ChangePasswordModal';
 export * from './MypageContentsWrap';
+export * from './MyPageInfo.tsx';
 export * from './MyPageInfoRow';
 export * from './MypageNav';
 export * from './MypageOutside';
