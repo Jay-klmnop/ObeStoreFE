1d3055b44e6439d7d0aa1271bdb2f3bbe5d7dc05
Jay <jiyeyoun@outlook.com>
2025-11-19 10:55:06 +0900

    chore: access 토큰을 기존 쿠키 방식에서 헤더로 변경, refresh는 쿠키로 처리 (#357)

--- src/features/auth/store/authstore.ts
+++ src/features/auth/store/authstore.ts
@@ -1,4 +1,4 @@
-import { authLogin, authSignup, authLogout, authRefreshToken } from '@/features/auth';
+import { authLogin, authSignup, authLogout } from '@/features/auth';
 import type { SignupFormData } from '@/features/auth';
 import { useFavoriteStore } from '@/features/favorite';
 import { useRewardStore } from '@/features/reward/store';
@@ -8,14 +8,12 @@ import { devtools, persist } from 'zustand/middleware';
 
 interface AuthState {
   access: string | null;
-  refresh: string | null;
   user: any | null;
-  setToken: (accessToken: string) => void;
+  setToken: (access: string) => void;
   setUser: (user: any) => void;
   signup: (data: SignupFormData) => Promise<void>;
   login: (email: string, password: string) => Promise<void>;
   logout: (navigate: NavigateFunction) => void;
-  refreshToken: () => Promise<void>;
 }
 
 export const useAuthStore = create<AuthState>()(
@@ -23,7 +21,6 @@ export const useAuthStore = create<AuthState>()(
     persist(
       (set, get) => ({
         access: null,
-        refresh: null,
         user: null,
         setToken: (access) => set({ access }),
         setUser: (user) => set({ user }),
@@ -38,10 +35,8 @@ export const useAuthStore = create<AuthState>()(
           await get().login(data.email, data.password);
         },
         login: async (email, password) => {
-          const res = await authLogin({ email, password });
-          const { access, refresh, user } = res.data;
-          console.log('로그인 성공, 새로운 상태 설정:', { access, user });
-          set({ access, refresh, user });
+          const { access, user } = await authLogin({ email, password });
+          set({ access, user });
         },
         logout: async (navigate) => {
           try {
@@ -49,24 +44,17 @@ export const useAuthStore = create<AuthState>()(
           } catch (error) {
             console.error('로그아웃 API 호출 실패:', error);
           } finally {
-            set({ access: null, refresh: null, user: null });
+            set({ access: null, user: null });
             useFavoriteStore.getState().reset();
             useRewardStore.getState().resetReward();
             navigate('/');
           }
-        },
-        refreshToken: async () => {
-          const { refresh } = get();
-          if (!refresh) return;
-          const res = await authRefreshToken({ refreshToken: refresh });
-          set({ access: res.data.access });
         },
       }),
       {
         name: 'auth-storage',
         partialize: (state) => ({
           access: state.access,
-          refresh: state.refresh,
           user: state.user,
         }),
       }
